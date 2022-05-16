369713170df9686fc86670e11eb7e66f61c26ccc
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-05-16 12:08:31 -0700

    Update Explanation.svelte

--- src/lib/Explanation.svelte
+++ src/lib/Explanation.svelte
@@ -13,9 +13,3 @@
 	<h2>{title}</h2>
 	<p><slot /></p>
 </section>
-
-<style lang="scss">
-	.selected {
-		box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.3), 0px 2px 6px 2px rgba(0, 0, 0, 0.15);
-	}
-</style>
